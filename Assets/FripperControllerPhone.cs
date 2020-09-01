2608895cbfd90c6caa2936290c2e94770d8523c0
Yulice-0603 <69956727+Yulice-0603@users.noreply.github.com>
2020-09-01 18:59:01 +0900

    Initial commit

---
+++ Assets/FripperControllerPhone.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperControllerPhone : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //マルチタッチ入力の判定
+        if (Input.touchCount > 0)
+        {
+            //タップの情報を取得
+            Touch[] myTouch = Input.touches;
+
+            for (int i =0; i < Input.touchCount; i++)
+            {
+                //左側でタップした時時左フリッパーを動かす
+                if (myTouch[i].position.x < 540 && tag == "LeftFripperTag" && myTouch[i].phase == TouchPhase.Began)
+                {
+                    SetAngle(this.flickAngle);
+                }
+
+                //右側でタップした時時左フリッパーを動かす
+                if (myTouch[i].position.x > 540 && tag == "RightFripperTag" && myTouch[i].phase == TouchPhase.Began)
+                {
+                    SetAngle(this.flickAngle);
+                }
+
+                //離れた時フリッパーを元に戻す
+                if (myTouch[i].position.x < 540 && tag == "LeftFripperTag" && myTouch[i].phase == TouchPhase.Ended)
+                {
+                    SetAngle(this.defaultAngle);
+                }
+                if (myTouch[i].position.x > 540 && tag == "RightFripperTag" && myTouch[i].phase == TouchPhase.Ended)
+                {
+                    SetAngle(this.defaultAngle);
+                }
+            }
+        }              
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
---
+++ Assets/PointCounter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointCounter : MonoBehaviour
+{
+    GameObject pointText;
+    //pointを初期化
+    private int point = 0;
+
+    //各オブジェクトの得点
+    int smallstarpoint = 10;
+    int largestarpoint = 15;
+    int smallcloudpoint = 20;
+    int largecloudpoint = 40;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        //Pointオブジェクトの取得
+        this.pointText = GameObject.Find("Point");
+        //最初は0で表示
+        this.pointText.GetComponent<Text>().text = point.ToString("D5");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        //ポイントの累積
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.point += this.smallstarpoint;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.point += this.largestarpoint;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            this.point += this.smallcloudpoint;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            this.point += this.largecloudpoint;
+        }
+
+        //累積したポイントの表示
+        this.pointText.GetComponent<Text>().text = point.ToString("D5");
+    }
+
+}
